093477b7f5bf01deb6ad89423b84eaabe59ba96e
YUYAO TU <142699145+spencertyy@users.noreply.github.com>
2026-05-20 17:04:27 -0600

    feat: improve sidebar UI layout and button styling, add the search btn

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,11 +1,17 @@
 import type { Conversation } from "../types/chat";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit } from "@fortawesome/free-regular-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 type SidebarProps = {
   conversations: Conversation[];
   activeConvId: string | null;
   setActiveConvId: (id: string) => void;
   handleNewChat: () => void;
   handleDeleteConv: (id: string) => void;
+  input: string;
+  setInput: (value: string) => void;
+  handleSend: () => void;
 };
 export default function Sidebar({
   conversations,
@@ -13,11 +19,34 @@ export default function Sidebar({
   handleNewChat,
   setActiveConvId,
   handleDeleteConv,
+  input,
+  setInput,
+  handleSend,
 }: SidebarProps) {
   return (
     <aside className="sidebar">
-      <button onClick={handleNewChat} className="newChat">
-        * New Chat
+      <div className="sidebar-header">
+        <button onClick={handleNewChat} className="newChat">
+          <FontAwesomeIcon icon={faEdit} />
+        </button>
+      </div>
+      <button className="searchBar">
+        <FontAwesomeIcon icon={faSearch} />
+        <input
+          className="search"
+          type="text"
+          placeholder="Search..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.nativeEvent.isComposing) return; //解决输入法问题
+
+            if (e.key === "Enter" && !e.shiftKey) {
+              e.preventDefault();
+              handleSend();
+            }
+          }}
+        />
       </button>
       <div className="conv-list">
         {conversations.map((conv) => (
@@ -34,7 +63,7 @@ export default function Sidebar({
                 handleDeleteConv(conv.id);
               }}
             >
-              ×
+              <FontAwesomeIcon icon={faTrash} />
             </button>
           </div>
         ))}
